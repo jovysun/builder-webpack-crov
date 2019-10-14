9a8fc240605f73313bc3e8a65212e870c632e4e8
jovysun <jovy_sun@163.com>
2019-10-14 19:19:33 +0800

    feat: first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  base: require('./lib/webpack.base'),
+  dev: require('./lib/webpack.dev'),
+  prod: require('./lib/webpack.prod'),
+  ssr: require('./lib/webpack.ssr'),
+}
---
+++ test/unit/webpack-base-test.js
@@ -0,0 +1,21 @@
+const assert = require('assert');
+
+describe('webpack.base.js test case', () => {
+  const baseConfig = require('../../lib/webpack.base');
+  // console.log(baseConfig);
+
+  it('entry', () => {
+    assert.equal(
+      baseConfig.entry.index.indexOf(
+        'builder-webpack-crov/test/smoke/template/src/index/index.js'
+      ) > -1,
+      true
+    );
+    assert.equal(
+      baseConfig.entry.search.indexOf(
+        'builder-webpack-crov/test/smoke/template/src/search/index.js'
+      ) > -1,
+      true
+    );
+  });
+});
